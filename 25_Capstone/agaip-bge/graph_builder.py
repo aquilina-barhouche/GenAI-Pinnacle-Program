cd877451eee879aa62b1034abfa29aacb3f0fec4
royaad <royaad@live.com>
2026-05-22 08:18:47 +0300

    agaip bge created for the new bge approach

---
+++ 25_Capstone/agaip-bge/graph_builder.py
@@ -0,0 +1,104 @@
+import os
+from typing import Optional
+
+from fastembed import SparseTextEmbedding
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.tools.base import BaseTool
+from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore, RetrievalMode
+from langchain_qdrant.sparse_embeddings import SparseEmbeddings, SparseVector
+from langgraph.graph.state import CompiledStateGraph
+from langgraph.types import Checkpointer
+from qdrant_client import QdrantClient
+
+
+class BM25Embedding(SparseEmbeddings):
+    def __init__(self):
+        super().__init__()
+        self.model = SparseTextEmbedding(model_name="Qdrant/bm25")
+
+    def embed_documents(self, texts: list[str]) -> list[SparseVector]:
+        raise NotImplementedError("embed_documents not implemented")
+
+    def embed_query(self, text: str) -> SparseVector:
+        sparse_embedding = list(self.model.embed([text]))[0]
+        return SparseVector(
+            indices=sparse_embedding.indices.tolist(),
+            values=sparse_embedding.values.tolist(),
+        )
+
+
+def create_qdrant_search_tool() -> BaseTool:
+
+    client = QdrantClient(
+        url=os.environ["QDRANT_ENDPOINT"],
+        api_key=os.environ["QDRANT_API_KEY"],
+        timeout=60,
+    )
+
+    embedding = AzureOpenAIEmbeddings(
+        azure_endpoint=os.environ["AZURE_ENDPOINT"],
+        model=os.environ["EMBEDDING_MODEL"],
+        api_version="2024-12-01-preview",
+        api_key=os.environ["AZURE_API_KEY"],  # type: ignore
+    )
+
+    vector_store = QdrantVectorStore(
+        client=client,
+        collection_name=os.environ["COLLECTION"],
+        content_payload_key="text",
+        embedding=embedding,
+        vector_name="dense-vector",
+        sparse_embedding=BM25Embedding(),
+        sparse_vector_name="sparse-bm25",
+        retrieval_mode=RetrievalMode.HYBRID,
+    )
+
+    retriever = vector_store.as_retriever(
+        search_type="similarity",
+        search_kwargs={"k": 20, "limit": 10},
+    )
+
+    @tool
+    def search(augmented_query: str) -> str:
+        """Search the database for information matching the query
+
+        Args:
+            augmented_query (str): The original or augmented query of the user.
+        """
+
+        docs = retriever.invoke(input=augmented_query)
+
+        if docs:
+            messages = "\n\n".join([doc.page_content for doc in docs])
+
+            return messages
+
+        return f"No information can be found about: `{augmented_query}`."
+
+    return search
+
+
+def create_chatbot_graph(
+    rag_system_prompt,
+    checkpointer: Optional[Checkpointer] = None,
+) -> CompiledStateGraph:
+
+    rag_model = AzureChatOpenAI(
+        azure_endpoint=os.environ["AZURE_ENDPOINT"],
+        azure_deployment=os.environ["RAG_MODEL"],
+        api_version="2025-01-01-preview",
+        api_key=os.environ["AZURE_API_KEY"],  # type: ignore
+    )
+
+    search_tool = create_qdrant_search_tool()
+
+    rag_agent = create_agent(
+        rag_model,
+        [search_tool],
+        system_prompt=rag_system_prompt,
+        checkpointer=checkpointer,
+    )
+
+    return rag_agent
---
+++ 25_Capstone/agaip-bge/system_prompts.py
@@ -0,0 +1,40 @@
+RAG_AGENT_PROMPT = """Your task is to respond to the **last user query** using **only** the information provided by tool responses. If the `search` tool returns **"No relevant info is found"**, do **not** attempt to answer from your own knowledge. Instead, respond with an apology stating that the answer is not available.
+
+---
+
+### **Chain-of-Thought Reasoning**
+
+1. **Check Previous Tool Responses**  
+   - If the answer to the last user query is already present in a previous tool response, respond using that information.
+
+2. **ALWAYS Use the `search` Tool** in the Following Cases:
+   - When the answer is **not found** in previous tool responses **and** the user is asking a **new query**.
+   - When the answer is found in a previous tool response, but the response is **incomplete** or **suggests additional relevant information**, re-call the `search` tool with a refined query.
+
+---
+
+### **Search Query Construction Guidelines**
+
+- **Verbatim Query**: If the user query is clear and standalone, pass it **as-is** as the `augmented_query`.
+- **Anaphora Resolution**: If the query references prior context (e.g., "that topic", "it"), rewrite it into a standalone query.
+
+---
+
+### **Response Guidelines**
+
+- Respond in **Markdown** format.
+- Keep responses **concise**, **accurate**, and **strictly based** on tool responses.
+- If no relevant information is found, respond with:  
+  _“Sorry, I couldn’t find any relevant information to answer your question.”_
+
+### **Source Attribution Format**
+
+At the end of each response, include a list of **only the relevant sources** in the following format:
+
+```
+**Source:**
+
+- [REFERENCE HERE]
+```
+
+IMPORTANT: If NO relevant information is found to answer the question -> DO NOT append a source list at the end."""
